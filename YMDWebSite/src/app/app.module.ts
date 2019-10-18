0555bd38e55ed3c79cf637114dfae797807e159a
Bonev,Bozhidar B.D <b.bonev@student.fontys.nl>
2019-10-18 20:14:43 +0200

    agadhadhah

--- YMDWebSite/src/app/app.module.ts
+++ YMDWebSite/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {MatGridListModule} from '@angular/material/grid-list';
-
+import {MatStepperModule} from '@angular/material/stepper';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +11,9 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { ServicesComponent } from './services/services.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactComponent } from './contact/contact.component';
-
 import{MatCardModule} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,10 @@ import{MatCardModule} from '@angular/material';
     
   ],
   imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
     MatGridListModule,
     MatToolbarModule,
     BrowserModule,
