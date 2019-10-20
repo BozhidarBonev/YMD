54db51a4bba997b5e0dde301d36820503c171434
Bonev,Bozhidar B.D <b.bonev@student.fontys.nl>
2019-10-20 13:13:53 +0200

    agagasg

--- YMDWebSite/src/app/aboutus/aboutus.component.ts
+++ YMDWebSite/src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 export interface Tile {
   color: string;
   cols: number;
@@ -8,16 +16,27 @@ export interface Tile {
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  styleUrls: ['./aboutus.component.css'],
+  animations: [
+    trigger('enterPic',[
+      state('initial', style({transform: 'translateX(0)',opacity:0})),
+      state('final', style({transform: 'translateX(300)',opacity:1})),
+      transition('initial=>final',animate(10000))
+    ])
+
+  ]
 })
 export class AboutusComponent implements OnInit {
     tiles: Tile[] = [
       {text: 'One', cols: 2, rows: 1, color: 'lightblue'},
       {text: 'Two', cols: 2, rows: 1, color: 'lightgreen'},
     ];
+    state="initial";
   constructor() { }
 
   ngOnInit() {
   }
-
+  PicAnim(text:string){
+    this.state = text;
+  }
 }
