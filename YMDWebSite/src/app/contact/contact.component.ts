89f608f2ea4b1a106bda58aa1ec21128724c830f
Bonev,Bozhidar B.D <b.bonev@student.fontys.nl>
2019-10-20 23:50:32 +0200

    hui

--- YMDWebSite/src/app/contact/contact.component.ts
+++ YMDWebSite/src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,29 +9,45 @@ export class ContactComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
   sender="";
   name="";
   message = "";
+  
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl:['', Validators.required]
+    });
   }
-  SetSender(email:string){
-    this.sender = email;
+  SetSender(){
+    this.sender = this.firstFormGroup.get('firstCtrl').value;
   }
-  SetName(name:string){
-    this.sender = name;
+  SetName(){
+    this.name =  this.secondFormGroup.get('secondCtrl').value;
   }
-  SetMessage(message:string){
-    this.sender = message;
+  SetMessage(){
+    this.message =  this.thirdFormGroup.get('thirdCtrl').value;
   }
-  Send(){
+  Send(){//TO DO
+    this.SetSender();
+    this.SetName();
+    this.SetMessage();
+    if(this.sender!="" && this.name!=""&& this.message!=""){
+      console.log( "Email: "+this.sender+" Name: "+this.name+" Message: " + this.message);
+      this.thirdFormGroup.reset();
+    }
+    else{
+      console.log("KUR");
+    }
     
   }
 }
